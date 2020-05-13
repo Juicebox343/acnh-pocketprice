1be00870f7aabc0b09ea8a7204caab564902d498
Joe <47640432+Juicebox343@users.noreply.github.com>
2020-05-13 00:12:12 -0700

    adding kofi

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,10 @@ class Header extends React.Component{
     render(){
         return(
             <header>
-
+                <div>
+                <a className='kofi' href='https://ko-fi.com/heiserj'><img src='./kofi2.png' alt='Kofi link for the developer'/></a>
+                </div>
+               
             </header>
         )
     }
